bf1f169129c6080f29a392d0257dffb93f1318ca
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2021-01-04 23:53:24 +0545

    Update Footer.js
    removed instagram footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,7 @@ import './Footer.css'
 
 export default () => (
   <div>
-    <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
-    </h2>
-    <br />
+   
     <footer className="footer">
       <div className="container taCenter">
         <span>
