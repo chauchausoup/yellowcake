f55f8512a3fee0ed83aa8d381be7dd929742945f
Krishna <7krishna7dhakal7@gmail.com>
2021-01-18 14:50:50 +0545

    more to go

--- src/components/HomePageContent.js
+++ src/components/HomePageContent.js
@@ -9,20 +9,53 @@ function HomePageContent(){
                     PROJECTS
                 </div>
                 <div className="projectHolders">
-                    <ul>
-                        <li>1</li>
-                        <li>2</li>
-                        <li>3</li>
-                        <li>4</li>
+                    <ul className="projectGroup">
+                        <li className="projectPer">
+                            <div className="projectTitle">
+                                Human Tracking and Reidentification
+                            </div>
+                            <div className="projectDesc">
+                                Made using YOLOV4,Bhattacharya Distance,Karlman Filter etc
+                            </div>
+                            <div className="projectStacks">
+                                react, python
+                            </div>
+                        
+                        </li>
+                        <li className="projectPer">2</li>
+                        <li className="projectPer">3</li>
+                        <li className="projectPer">4</li>
+                        <li className="projectPer">5</li>
+                        <li className="projectPer">6</li>
+                        <li className="projectPer">7</li>
                     </ul>
                 </div>
             </div>
             <div className="skills">
-                skills
-
+            <div className="skillPlaceholder">
+                    SKILLS
+                </div>
+                <div className="skillHolders">
+                    <ul className="skillGroup">
+                        <li className="skillPer">1</li>
+                        <li className="skillPer">2</li>
+                        <li className="skillPer">3</li>
+                        <li className="skillPer">4</li>
+                    </ul>
+                </div>
             </div>
             <div className="socials">
-                socials
+            <div className="socialPlaceholder">
+                    SOCIALS
+                </div>
+                <div className="socialHolders">
+                    <ul className="socialGroup">
+                        <li className="socialPer">1</li>
+                        <li className="socialPer">2</li>
+                        <li className="socialPer">3</li>
+                        <li className="socialPer">4</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
